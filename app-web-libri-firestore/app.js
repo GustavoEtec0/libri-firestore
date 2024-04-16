8bc390708118e09dd6cac15cef4ea7d5339ecb23
GustavoEtec0 <160188621+GustavoEtec0@users.noreply.github.com>
2024-04-16 20:32:15 -0300

    crud firestore

--- app-web-libri-firestore/app.js
+++ app-web-libri-firestore/app.js
@@ -1,7 +1,42 @@
-db.collection("app-libri-gustavo")
+let livrosList = document.querySelector("#book-list");
+function renderbook(doc) {
+  let li = document.createElement("li");
+  let titulo = document.createElement("span");
+  let autor = document.createElement("span");
+
+  li.setAttribute("data-id", doc.id);
+  titulo.textContent = doc.data().titulo;
+  autor.textContent = doc.data().autor;
+
+  //adicionando dos dados de autor e titulo na tag
+
+  li.appendChild(titulo);
+  li.appendChild(autor);
+  livrosList.appendChild(li);
+}
+
+db.collection("libri-data")
   .get()
   .then((snepshot) => {
     snepshot.docs.forEach((doc) => {
-      console.log(doc.data());
+      console.log(doc.data(), " teste");
+      renderbook(doc);
     });
   });
+
+const form = document.querySelector("#add-book-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  db.collection("app-libri-gustavo")
+    .add({
+      autor: form.autor.value,
+      livro: form.livro.value,
+    })
+    .then(() => {
+      form.autor.value = "";
+      form.livro.value = "";
+      window.location.reload();
+    });
+});
