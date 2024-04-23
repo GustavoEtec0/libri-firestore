7657b2d9593954d43a3b6a8bb08a851e38c60337
GustavoEtec0 <160188621+GustavoEtec0@users.noreply.github.com>
2024-04-23 20:16:38 -0300

    delete

--- app-web-libri-firestore/app.js
+++ app-web-libri-firestore/app.js
@@ -1,42 +1,58 @@
-let livrosList = document.querySelector("#book-list");
+let titulosList = document.querySelector('#book-list')
 function renderbook(doc) {
-  let li = document.createElement("li");
-  let titulo = document.createElement("span");
-  let autor = document.createElement("span");
+    let li = document.createElement('li')
+    let titulo = document.createElement('span')
+    let autor = document.createElement('span')
+    let excluir = document.createElement('div')
 
-  li.setAttribute("data-id", doc.id);
-  titulo.textContent = doc.data().titulo;
-  autor.textContent = doc.data().autor;
+    excluir.textContent = 'x'
 
-  //adicionando dos dados de autor e titulo na tag
+    li.setAttribute('data-id', doc.id)
+    titulo.textContent = doc.data().titulo
+    autor.textContent = doc.data().autor
 
-  li.appendChild(titulo);
-  li.appendChild(autor);
-  livrosList.appendChild(li);
+    //adicionando dos dados de autor e titulo na tag
+
+    li.appendChild(titulo)
+    li.appendChild(autor)
+    li.appendChild(excluir)
+    //Trava açao no click do botao x pra exclusao do arquivo
+    excluir.addEventListener('click', (event) => {
+        event.stopPropagation()
+        let id = event.target.parentElement.getAttribute('data-id')
+        // alert(id)
+        db.collection('libri-data')
+            .doc(id)
+            .delete()
+            .then(() => {
+                window.location.reload()
+            })
+    })
+    titulosList.appendChild(li)
 }
 
-db.collection("libri-data")
-  .get()
-  .then((snepshot) => {
-    snepshot.docs.forEach((doc) => {
-      console.log(doc.data(), " teste");
-      renderbook(doc);
-    });
-  });
-
-const form = document.querySelector("#add-book-form");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  db.collection("app-libri-gustavo")
-    .add({
-      autor: form.autor.value,
-      livro: form.livro.value,
+db.collection('libri-data')
+    .get()
+    .then((snepshot) => {
+        snepshot.docs.forEach((doc) => {
+            console.log(doc.data(), ' teste')
+            renderbook(doc)
+        })
     })
-    .then(() => {
-      form.autor.value = "";
-      form.livro.value = "";
-      window.location.reload();
-    });
-});
+
+const form = document.querySelector('#add-book-form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    db.collection('libri-data')
+        .add({
+            autor: form.autor.value,
+            titulo: form.titulo.value,
+        })
+        .then(() => {
+            form.autor.value = ''
+            form.titulo.value = ''
+            window.location.reload()
+        })
+})
